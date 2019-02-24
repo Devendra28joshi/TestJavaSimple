030c27cd8259c97ffabbba8ebf05a0c704df1908
Devendra28joshi <mail.devendra.joshi@gmail.com>
2019-02-24 22:49:45 +0530

    Test

---
+++ Java-cucumber/src/test/java/Java_cucumber/Java_cucumber/Test.java
@@ -0,0 +1,16 @@
+package Java_cucumber.Java_cucumber;
+
+public class Test 
+{
+
+	@org.testng.annotations.Test
+	void getData()
+	{
+		System.out.println("Test java xml");
+		System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx");
+		System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx");
+		System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx");
+		System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXx");
+		
+	}
+}
